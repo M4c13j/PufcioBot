9ef9f8331948671de86af98b974d4e7710cacf0d
M4c13j <maciekcx11@gmail.com>
2022-10-24 23:49:47 +0200

    nothing

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 import os
-from os.path import join, dirname
+# from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv( dotenv_path )
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
